9e4902817aaf46f8848fdd8223f56fb83fdaaf49
Gopal Singh <109897876+GopalSingh09@users.noreply.github.com>
2023-03-18 23:12:19 +0530

    Add files via upload

---
+++ pythonDay5.py
@@ -0,0 +1,35 @@
+#                       chapter= 10  If else and elif condition in python
+# operators in python
+#assignment operators
+#x = 10
+#print(x)
+#x+=3
+#print(x)
+#x-=3
+#print(x)
+#x%=3
+#print(x)
+
+#if else elif
+
+x = 20
+y = 15
+z = 10
+p = 10
+
+if x > y:
+    print("x is greator than y")
+else:
+    print("y is greator than x")
+
+if y == z:
+    print("x is equal to y")
+elif y > z:
+    print("y is greator than z")
+else:
+    print("none")
+
+if x > z and p == z:
+    print("x is greator than p and z")
+else:
+    print("x is not greator than p and z")
