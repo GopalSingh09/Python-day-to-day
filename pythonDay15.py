c05de2bbf711bf939387c8de38ba61c72f3a523a
Gopal Singh <109897876+GopalSingh09@users.noreply.github.com>
2023-03-19 00:48:29 +0530

    Add files via upload

---
+++ pythonDay15.py
@@ -0,0 +1,49 @@
+#                                               Day 15
+#                               Chapter: 26 Using External and built in module
+
+# import random
+#
+# random_num = random.randint(0, 10)    # gives you any random integer in between those number including them
+# print(random_num)
+# random_range = random.randrange(10)   #gives you any random range
+# print(random_range)
+#
+# rand = random.random()       #gives you the random number not integer, from 0 to 1 , to increase value simply
+# print(rand)                       #multiply it with it
+#
+# rand1 = random.random() * 100
+# print(rand1)
+#
+# # We have a list,
+# list1 = ["Hungama", "Cartoon network", "Disney", "Pogo", "nicklodeans"]
+# choice = random.choice(list1)   # it return the any random item from the list to choice
+#
+# print(choice)
+
+#test
+
+# import platform
+#
+# x = platform.machine()  # give you  machine type
+# print("Machine type: ", x)
+#
+# y = platform.node()     #give you computer network name
+# print("network name: ", y)
+#
+# z = platform.platform()   # give platform details
+# print("platform: ", z)
+#
+# a = platform.processor()  #give full processor name
+# print("Processor name: ", a)
+#
+# b = platform.python_build() #give py build no. and date
+# print("Build date and number: ", b)
+#
+# c = platform.python_compiler()  #give py compiler name
+# print("Compiler name: ", c)
+#
+# d = platform.version()  #give py version name
+# print("version: ", d)
+
+
+
