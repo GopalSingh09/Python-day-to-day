acaeae35b89d7a06d674a1029caacc7c836f03d5
Gopal Singh <109897876+GopalSingh09@users.noreply.github.com>
2023-03-18 23:09:59 +0530

    Add files via upload

---
+++ pythonDay1.py
@@ -0,0 +1,51 @@
+print("dragon ball z")
+#please work after 2 lines.
+
+
+print("I'll do it later.")
+"""
+multi line comment syntax.
+you can use this 
+space as 
+comment section
+"""
+"""This is also multi line comment syntax"""
+#type of print uses
+#type 1
+print("You are allowrd")
+print("but at a condition")
+
+#type 2
+print("you are allowed", end=" ")
+print("at one condition")
+
+#type 3
+print("you are allowed ", end="but ")
+print("at one condition")
+
+#type 4
+print("you are allowed","but at a condition")
+
+#way to write if a escape eq. is comming
+print("c:\nostagonist") #not
+
+print("c:\\nostagonist") #like this
+print("c:\"nostagonist")
+
+#some escape seq.
+#new line
+print("Welcome to this\nshop")
+#horizontal tab
+print("welcome to this\tshop")
+#backslash
+print("c:\\aaolala")
+#backspace
+print("interview is scheduledo\b")
+#hexa value hh
+print("\x48\x45\4C\4c")
+#octal value
+print("\110\105\114\114\117\040")
+#carriage return
+print(r"hello\tworld\n you are welcome")
+#
+
