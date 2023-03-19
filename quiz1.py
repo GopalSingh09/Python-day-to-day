16e15e81201d843099e374860e977ba7388817eb
Gopal Singh <109897876+GopalSingh09@users.noreply.github.com>
2023-03-20 02:14:33 +0530

    Add files via upload

---
+++ quiz1.py
@@ -0,0 +1,12 @@
+#                                               quize 1
+# A simple 2 digit adder calculator
+
+print("\t\t\t Welcome to the two digit adding calculator\n\n")
+print("Enter your first value:")
+value1 = input()
+print("Enter your second value:")
+value2 = input()
+print("\nYour calculation is:")
+value3 = int(value1) + int(value2)
+print(value1, "+", value2, "=", value3)
+
